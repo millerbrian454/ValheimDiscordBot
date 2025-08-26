d62f511ff868928fc9cb43604776658a90ab0087
Brian Miller <34964641+millerbrian454@users.noreply.github.com>
2025-08-26 19:07:39 -0400

    implemented server online checker

---
+++ ValheimDiscordBot/Contracts/ServerStatus.cs
@@ -0,0 +1,15 @@
+﻿using ValheimDiscordBot.Services;
+
+namespace ValheimDiscordBot.Contracts
+{
+    public class ServerStatus
+    {
+        public bool IsOnline { get; set; }
+        public bool ProcessRunning { get; set; }
+        public bool PortOpen { get; set; }
+        public int? ResponseTime { get; set; }
+        public DateTime LastChecked { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public ProcessInfo? ProcessInfo { get; set; }
+    }
+}
